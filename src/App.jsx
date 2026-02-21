f3641e30073359c4503f8488d41c7115aac74268
Shelomith254 <shelomith42@gmail.com>
2026-02-21 19:20:29 +0300

    Fix App.jsx merge conflict and clean routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Skills from "./pages/Skills";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
+    <Router>
+      <div className="app">
+        <Routes>
+
+          {/* Home Page */}
+          <Route path="/" element={<Home />} />
+
+          {/* About Page */}
+          <Route path="/about" element={<About />} />
+
+          {/* Skills Page */}
+          <Route path="/skills" element={<Skills />} />
+
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
